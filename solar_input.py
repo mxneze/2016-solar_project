5e606288a4856fdfe69a7b7aa3e0da3e05de8db9
Yestove <vbc60780@gmail.com>
2022-11-22 10:13:03 +0300

    first try

--- solar_input.py
+++ solar_input.py
@@ -23,6 +23,10 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +47,16 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-
+    star.type = line[0]
+    star.r = line[1]
+    star.color = line[2]
+    star.m = line[3]
+    star.x = line[4]
+    star.y = line[5]
+    star.Vx = line[6]
+    star.Vy = line[7]
     pass  # FIXME: not done yet
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,6 +73,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
+    planet.type = line[0]
+    planet.r = line[1]
+    planet.color = line[2]
+    planet.m = line[3]
+    planet.x = line[4]
+    planet.y = line[5]
+    planet.Vx = line[6]
+    planet.Vy = line[7]
     pass  # FIXME: not done yet...
 
 
@@ -78,9 +98,18 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            output_filename.writelines(obj.type, " ",
+                                       obj.r, " ",
+                                       obj.color, " ",
+                                       obj.m, " ",
+                                       obj.x, " ",
+                                       obj.y, " ",
+                                       obj.Vx, " ",
+                                       obj.Vy)
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,10 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += (gravitational_constant * obj.m * body.m / r**2) * (body.x - obj.x) / r
+        # FIXME: нужно вывести формулу...
+        body.Fy += (gravitational_constant * obj.m * body.m / r**2) * (body.y - obj.y) / r
+        # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
@@ -32,9 +34,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += body.Vx*dt  # FIXME: не понимаю как менять...
     body.Vx += ax*dt
     # FIXME: not done recalculation of y coordinate!
+    ay = body.Fy/body.m
+    body.y += body.Vy*dt
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
